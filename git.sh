1a0b52be83805ee030d097b9fba8e114ba09d9f3
Romain <romain.dalichamp@free.fr>
2019-09-12 20:11:39 -0400

    init commit

---
+++ git.sh
@@ -0,0 +1,9 @@
+git init
+git add origin "http://...."
+git pull origin master
+git add --all
+git commit -m "commit infos"
+git push origin master
+git branch <branch_name>
+git checkout <branch_name> #or git switch + git fetch
+git merge <branch_name>	#from master branch
